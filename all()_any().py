31cd448aed51125416652849bc9bdc9ea50041f0
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-09 02:20:00 +0400

    all() ve any() fonksiyonları

---
+++ all()_any().py
@@ -0,0 +1,9 @@
+# all() elimizde olan degerlerin hepsi "TRUE" ise True deger dondurur
+# bir tane "False" varsa bile False deger dondurecek
+liste=[0,1,2,3,4]
+print(all(liste))
+
+# Any()- elimizde olan degerlerin bir tanesi bile "True" ise True donderir
+# hepsi false oldugu zaman False deger dondurur
+
+print(any(liste))
